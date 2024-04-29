32c3221682849399c9fd183f6b4c684bdb312328
saketh145 <saketharamabadam092@gmail.com>
2024-04-29 15:33:05 +0530

    modified:   __pycache__/load.cpython-311.pyc 	new file:   __pycache__/website_data.cpython-311.pyc 	modified:   load.py 	modified:   app.py 	new file:   website_data.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import textwrap
 import google.generativeai as genai
 import streamlit as st
 import load
+import website_data
 st.header("Welcome to the summarize easy")
 url=st.text_input("Enter the url",placeholder="URL....")
 if url:
-    response=(load.output(url))
+    response=(load.output(website_data.scrape(url)))
     for chunk in response:
         st.write(chunk.text)
