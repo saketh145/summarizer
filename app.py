02896cf3b0d3a2bce36c0da28967f7f68b2cf90f
saketh145 <saketharamabadam092@gmail.com>
2024-04-29 15:07:59 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 import streamlit as st
 import load
 st.header("Welcome to the summarize easy")
-url=st.text_input("Enter the query")
+url=st.text_input("Enter the url",placeholder="URL....")
 if url:
     response=(load.output(url))
     for chunk in response:
