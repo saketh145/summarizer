90a28ee071a9e8ff65d05666a29d4e85d75edd1e
saketh145 <saketharamabadam092@gmail.com>
2024-04-30 07:51:00 +0530

    modified:   __pycache__/load.cpython-311.pyc 	modified:   app.py 	new file:   pages/document.py 	new file:   pages/text.py 	new file:   pages/url.py 	new file:   pages/video.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-import pathlib
-import textwrap
-import google.generativeai as genai
 import streamlit as st
-import load
-import website_data
+import streamlit_authenticator as stauth
 st.header("Welcome to the summarize easy")
-url=st.text_input("Enter the url",placeholder="URL....")
-if url:
-    response=(load.output(website_data.scrape(url)))
-    for chunk in response:
-        st.write(chunk.text)
+st.markdown("# Home")
+st.page_link("pages/url.py",label="Summarize with a url")
