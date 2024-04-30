7773cfe63a293ee04d987bef13b52e7585d6a0ad
saketh145 <saketharamabadam092@gmail.com>
2024-04-30 13:58:16 +0530

    new file:   __pycache__/document_data.cpython-311.pyc 	modified:   __pycache__/load.cpython-311.pyc 	modified:   app.py 	new file:   document_data.py 	modified:   pages/document.py 	modified:   pages/text.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 st.header("Welcome to the summarize easy")
-st.markdown("# Home")
 st.page_link("pages/url.py",label="Summarize with a url")
