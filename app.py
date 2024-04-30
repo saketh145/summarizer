dce5401bab2378d9f7466fd1ef90672cbb512ffa
saketh145 <saketharamabadam092@gmail.com>
2024-04-30 16:19:56 +0530

    modified:   app.py 	new file:   pages/youtube_video.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 import streamlit_authenticator as stauth
-st.header("Welcome to the summarize easy")
-st.page_link("pages/url.py",label="Summarize with a url")
+st.set_page_config(
+        page_title="Summarize EAZY",
+        page_icon="fav2.jpg",
+        layout="wide",
+)
+st.header("Welcome to the *SUMMARIZE EAZY*")
